7be8b2c2de20dc22a7c8317026d6fd4bfd3a9eb8
J. Austin Hughey <jah@jah.io>
2020-05-23 05:50:59 -0600

    Make User a top level namespace

---
+++ lib/console.rb
@@ -0,0 +1,8 @@
+class Minion
+  module Console
+    def self.start
+      puts "Welcome to the Minion application console (based on 'pry')."
+      binding.pry
+    end
+  end
+end
--- lib/models/user.rb
+++ lib/models/user.rb
@@ -1,36 +1,26 @@
-require 'rethinkdb'
-require 'json'
-require 'pry'
+class User
+  # Using this gives us to_json and from_json
+  include Minion::Model
 
-require_relative File.join('..', 'jsonable.rb')
+  # Define the attributes for this particular object
+  attr_accessor :id, :name, :email, :created_at
 
-class Minion
-  class User
-    # Using this gives us to_json and from_json
-    include Minion::JSONAble
+  def initialize(args = {email: nil, name: nil, created_at: nil})
+    @name = args[:name]
+    @email = args[:email]
+    @created_at = args[:created_at]
+  end
 
-    # Define the attributes for this particular object
-    attr_accessor :id, :name, :email, :created_at
+  def self.find_by_email(email)
+  end
 
-    def initialize(args = {email: nil, name: nil, created_at: nil})
-      @name = args[:name]
-      @email = args[:email]
-      @created_at = args[:created_at]
-    end
+  def self.create(u)
+    u.created_at = Time.now.utc
+  end
 
-    def self.find_by_email(email)
-    end
+  def destroy
+  end
 
-    def self.create(u)
-      u.created_at = Time.now.utc
-    end
-
-    def destroy
-    end
-
-    def valid?
-    end
-
+  def valid?
   end
 end
-binding.pry
