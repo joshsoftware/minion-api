f7647f4a881c142ebcf6493f5011f57780d4bfd6
J. Austin Hughey <jah@jah.io>
2020-05-23 19:26:27 -0600

    Move common model functionality into mixin

--- lib/model.rb
+++ lib/model.rb
@@ -1,19 +1,82 @@
 module Minion
   module Model
-    def to_json
-      hsh = {}
-      self.instance_variables.each do |v|
-        hsh["#{v}".gsub(/@/, '')] = self.instance_variable_get v
-      end
-      return hsh.to_json
+    def self.included base
+      base.send :include, InstanceMethods
+      base.extend ClassMethods
     end
 
-    def from_json(str)
-      hsh = JSON.load(str)
-      self.instance_variables.each do |var|
-        self.instance_variable_set var, hsh[var.to_s.gsub(/@/, '')]
+    module InstanceMethods
+      def to_json
+        hsh = {}
+        self.instance_variables.each do |v|
+          hsh["#{v}".gsub(/@/, '')] = self.instance_variable_get v
+        end
+        return hsh.to_json
       end
-      return self
+
+      def destroy
+        $pool.with do |conn|
+          self.class.r.table(self.class::TABLE).get(self.id).delete().run(conn)
+        end
+      end
+    end
+
+    module ClassMethods
+      def count
+        $pool.with do |conn|
+          r.table(self::TABLE).count().run(conn)
+        end
+      end
+
+      def from_json(str)
+        hsh = JSON.load(str)
+        thing = self.new
+        thing.instance_variables.each do |var|
+          thing.instance_variable_set var, hsh[var.to_s.gsub(/@/, '')]
+        end
+        return thing
+      end
+
+      def find(id)
+        $pool.with do |conn|
+          return self.from_json(r.table(self::TABLE).get(id).run(conn).to_json)
+        end
+      end
+
+      def create(thing)
+        # First, look for protected keys and remove their values
+        thing.class::PROTECTED_METHODS.each do |prot|
+          if thing.instance_variables.include?(prot)
+            thing.instance_variable_set(prot, nil)
+          end
+        end
+
+        # Set the created_at attribute to now
+        if thing.instance_variables.include?(:@created_at)
+          thing.created_at = Time.now.utc
+        end
+
+        hsh = JSON.parse(thing.to_json)
+        hsh.delete('id')
+        $pool.with do |conn|
+          id = r.table(thing.class::TABLE).insert(hsh).run(conn)['generated_keys'][0]
+          return thing.class.find(id)
+        end
+        # TODO: Somehow get the user or at least their ID back from this stmt
+      end
+
+      def r(*args)
+        args == [] ? RethinkDB::RQL.new : RethinkDB::RQL.new.expr(*args)
+      end
     end
   end
+end
+
+
+def self.find(id)
+  $pool.with do |conn|
+    u = User.from_json(r.table("users").get(id).run(conn).to_json)
+    return u
+  end
+  # return User.new.from_json((r.table("users").get(id).run($r)).to_json)
 end
--- lib/models/user.rb
+++ lib/models/user.rb
@@ -1,4 +1,8 @@
 class User
+  # Declare the table we'll use and methods only the app can set
+  TABLE = "users"
+  PROTECTED_METHODS = [:@id, :@created_at]
+
   # Using this gives us to_json and from_json
   include Minion::Model
 
@@ -13,29 +17,16 @@ class User
   end
 
   def self.find_by_email(email)
-    c = r.table('users').filter { |user| user['email'].eq(email) }.limit(1).run($r)
-    return User.new.from_json(c.first.to_json)
-  end
-
-  def self.find(id)
-    return User.new.from_json((r.table("users").get(id).run($r)).to_json)
-  end
-
-  def self.create(u)
-    # Don't allow spoofing IDs or creation time
-    u.created_at = Time.now.utc
-    u.id = nil
-
-    # Dump it in there
-    r.table('users').insert(u.to_json)
-
-    # Now find it again and return it
-    return User.find_by_email(u.email)
-  end
-
-  def destroy
+    $pool.with do |conn|
+      cursor = r.table('users').filter do |user|
+        user['email'].eq(email)
+      end.limit(1).run(conn)
+      return User.from_json(cursor.first.to_json)
+    end
   end
 
   def valid?
+    # Stop-gap for now, should be a real validation sometime later
+    true
   end
 end
