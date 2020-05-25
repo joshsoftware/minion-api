0a4b8637c73a1ba6aba420294b05830b9f48f661
J. Austin Hughey <jah@jah.io>
2020-05-24 23:14:54 -0600

    Hacktastic working subscribe for new commands

--- init.rb
+++ init.rb
@@ -5,9 +5,18 @@
 
 require 'active_support'
 require "active_support/core_ext/hash/indifferent_access"
+require "active_support/time_with_zone"
+require "active_support/core_ext/object/blank"
+require "active_support/core_ext/object/json"
+require "active_support/core_ext/time/acts_like"
+require "active_support/core_ext/time/calculations"
+require "active_support/core_ext/time/compatibility"
+require "active_support/core_ext/time/conversions"
+require "active_support/core_ext/time/zones"
 require 'connection_pool'
 require 'dry-struct'
 require 'em-websocket'
+require 'eventmachine'
 require 'fileutils'
 require 'grape'
 require 'json'
--- service/service.rb
+++ service/service.rb
@@ -1,8 +1,37 @@
-require 'em-websocket'
-
 module Minion
   module Service
     def self.start
+=begin
+    # module TLSHandler
+    #   def post_init
+    #     start_tls(:private_key_file => '/tmp/server.key', :cert_chain_file => '/tmp/server.crt', :verify_peer => false)
+    #   end
+    # end
+
+    App = lambda do |env|
+      if Faye::WebSocket.websocket?(env)
+        ws = Faye::WebSocket.new(env)
+
+        ws.on :message do |event|
+          ws.send(event.data)
+        end
+
+        ws.on :close do |event|
+          p [:close, event.code, event.reason]
+          ws = nil
+        end
+
+        # Return async Rack response
+        ws.rack_response
+
+      else
+        # Normal HTTP request
+        [200, { 'Content-Type' => 'text/plain' }, ['Hello']]
+      end
+    end
+  end
+end
+=end
       EM.run {
         EM::WebSocket.start(
           :host => "0.0.0.0",
@@ -15,28 +44,59 @@ module Minion
         ) do |ws|
           ws.onopen { |handshake|
             puts "WebSocket connection opened from #{ws.remote_ip}"
-
-            # Access properties on the EM::WebSocket::Handshake object, e.g.
-            # path, query_string, origin, headers
-
-            # Publish message to the client
-            # ws.send "Hello Client, you connected to #{handshake.path}"
           }
 
-          ws.onclose { puts "Connection closed" }
+          ws.onclose { puts "WebSocket Connection closed for #{ws.remote_ip}" }
 
           ws.onmessage { |msg|
-            # puts "Recieved message: #{msg}"
-            # ws.send "Pong: #{msg}"
-            operation = proc {
-              # TODO: Have the operation update the database
-              # and return the new object in a literal 'return' statement
-              Minion::Service.update_db
-            }
+            # Parse the message (JSON) and if it's a subscribe, listen for
+            # changes to the commands table where its server ID is this server's
+            # id. Otherwise, it's going to be an add line command, so we need
+            # to add that to the database...which will trigger an update to
+            # the first situation.
+            puts "Recieved message from #{ws.remote_ip}: #{msg}"
+            message = JSON.parse(msg).deep_symbolize_keys
+            case message[:action]
+            when 'subscribe'
+              # TODO: Authenticate the request and verify identity so you can't
+              # subscribe to changes for somebody else's server.
+              # Client wants to be notified when there are new commands
+              # to be executed.
+              operation = proc {
+                $pool.with do |conn|
+# binding.pry
+                EM.run {
+                  RethinkDB::RQL.new.table('commands').changes.run(conn).each { |cmd| puts cmd; ws.send cmd.to_json }
+                }
+                  # RethinkDB::RQL.new.table('commands').filter do |cmd|
+                  #   (
+                  #     cmd['server_id'].eq(message[:server_id]) &&
+                  #     cmd['completed_at'].eq(nil) &&
+                  #     cmd['started_at'].eq(nil) &&
+                  #     cmd['issued_at'].gt(Time.now.utc - 5.minutes)
+                  #   )
+                  # end.changes.run(conn).each { |cmd|
+                  #   ws.send cmd.to_json
+                  # }
+                end
+              }
+            when 'update'
+              # Client is adding new output to a command. Look for the
+              # command by ID and update it accordingly (command.add_line)
+              operation = proc {
+                command = Command.find(message[:command_id])
+                # TODO: Make sure the command's server ID matches this server's id
+                [:stdout, :stderr].each do |dev|
+                  command.add_line(message[dev])
+                end
+              }
+            else
+              operation = {}
+            end
 
             callback = proc { |result|
               # TODO: Send result to websocket client (result should be hash)
-              ws.send result.to_json
+              ws.send "Result: #{result.to_json}"
             }
 
             # TODO: Try to figure out the error. If it's a disconnect or
@@ -50,24 +110,6 @@ module Minion
           }
         end
       }
-    end
-
-    def self.update_db
-      # This is a spoofed db update method for the time being.
-      hsh = {
-        command: {
-          id: "abc123",
-          initiated_at: Time.now.utc,
-          stdout: {
-            lines: [
-              { output: "asdfasdf asdfasdfa asfd...", at: Time.now.utc },
-              { output: "asdfasdf asdfasdfa asfd...", at: Time.now.utc },
-              { output: "asdfasdf asdfasdfa asfd...", at: Time.now.utc },
-            ]
-          }
-        }
-      }
-      return hsh
     end
   end
 end
