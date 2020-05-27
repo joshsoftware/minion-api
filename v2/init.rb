a5801881c303fc99ba3a913bf45f7188252a1bb1
J. Austin Hughey <jah@jah.io>
2020-05-27 04:55:33 -0600

    preliminary research on v2

---
+++ v2/init.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'async/websocket/adapters/rack'
+require 'async/io/stream'
+require 'async/http/endpoint'
+require 'async/websocket/client'
+require 'pry'
+require 'set'
