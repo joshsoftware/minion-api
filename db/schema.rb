6f9a533e125b080d6a8a77183b4e5af65087a4dd
Kirk Haines <wyhaines@gmail.com>
2020-10-07 09:50:09 -0600

    Update the count_estimate function to return a bigint.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_07_151522) do
+ActiveRecord::Schema.define(version: 2020_10_07_154751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
