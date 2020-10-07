c3879e0f2a04fbac2105a40fc6a7f3a3ff17604b
Kirk Haines <wyhaines@gmail.com>
2020-10-07 09:58:51 -0600

    Actually push the migration...

---
+++ db/migrate/20201007154751_modify_count_estimates_function_to_bigint.rb
@@ -0,0 +1,20 @@
+class ModifyCountEstimatesFunctionToBigint < ActiveRecord::Migration[6.0]
+  def change
+    execute <<-ESQL
+    DROP FUNCTION count_estimate;
+    -- Recreate it using a bigint as the return type.
+    CREATE FUNCTION count_estimate(query text) RETURNS integer AS $$
+      DECLARE
+        rec   record;
+        rows  bigint;
+      BEGIN
+        FOR rec IN EXECUTE 'EXPLAIN ' || query LOOP
+          rows := substring(rec."QUERY PLAN" FROM ' rows=([[:digit:]]+)');
+          EXIT WHEN rows IS NOT NULL;
+        END LOOP;
+        RETURN rows;
+      END;
+    $$ LANGUAGE plpgsql VOLATILE STRICT;
+    ESQL
+  end
+end
